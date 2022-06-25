ca0c7196482c2e5e532952a7c1807a256eaa735e
UL xD <whelidias@hotmail.com>
2022-06-25 06:27:36 -0300

    (feat): users page add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,38 @@
 import type { NextPage } from 'next'
-
+import { Flex, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+import { Input } from '../components/Form/input'
 
 const Home: NextPage = () => {
   return (
-    <h1>
-      DashGo
-    </h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align='center'
+      justify='center'
+    >
+      <Flex
+        as='form'
+        w='100%'
+        maxWidth={360}
+        bg='gray.800'
+        p='8'
+        flexDir='column'
+      >
+        <Stack spacing={4}>
+          <Input
+            type='email'
+            name='email'
+            label= 'Email'
+          />
+          <Input
+            type='password'
+            name='password'
+            label= 'Senha'
+          />
+        </Stack>
+        <Button type='submit' size='lg' mt='6' colorScheme='pink'>Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
 
